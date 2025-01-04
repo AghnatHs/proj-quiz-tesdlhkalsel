6597551fb5e3533575e4692e6bb9f08eff2924a6
AghnatHs <aghnathasya@gmail.com>
2025-01-04 20:52:31 +0700

    - get all questions -delete question by id

--- src/database/adminDashboard.query.mjs
+++ src/database/adminDashboard.query.mjs
@@ -5,6 +5,23 @@ const AdminDashboardQuery = {
     const [results] = await db.query(`SELECT * FROM sessions`);
     return results;
   },
+  getAllQuestions: async function () {
+    const [results] = await db.query(
+      `SELECT  
+      id,
+      content,
+      option1,
+      option2,
+      option3,
+      option4,
+      option5,
+      option_correct,
+      subject,
+      difficulty
+      FROM questions`
+    );
+    return results;
+  },
   createNewQuestion: async function ({
     content,
     option1,
@@ -33,6 +50,12 @@ const AdminDashboardQuery = {
         difficulty,
       ]
     );
+    return results;
+  },
+  deleteQuestionById: async function (questionId) {
+    const [results] = await db.query(`DELETE FROM questions WHERE id = ?`, [
+      questionId,
+    ]);
     return results;
   },
 };
